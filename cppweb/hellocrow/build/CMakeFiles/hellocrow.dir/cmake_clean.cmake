3495b163f241be5af10ff748e94b8511f64d30e9
Anand Kumar Dubey <dubey.anandkr@gmail.com>
2021-09-03 19:06:31 +0530

    Learning directory init

---
+++ cppweb/hellocrow/build/CMakeFiles/hellocrow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hellocrow.dir/main.cpp.o"
+  "hellocrow.pdb"
+  "hellocrow"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hellocrow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
